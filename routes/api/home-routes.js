666a2f1e2fc38f9417e277726442b65f82cf3d47
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-03-29 14:02:30 -0600

    Render home page

--- routes/api/home-routes.js
+++ routes/api/home-routes.js
@@ -5,7 +5,8 @@ const { Blog } = require('../../models');
 router.get('/', async (req, res) => {
     try {
       const dbBLogData = await Blog.findAll();
-      res.render('main', { dbBLogData });
+      const blogs= dbBLogData.map((blog) => blog.get({ plain: true }));
+    res.render('dashboard', { blogs });
     //  return  res.status(200).json(dbBLogData);
     } catch (err) {
      return  res.status(500).json(err);
