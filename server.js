7f11cd33d3b5b0c1fc0395e5bbb6997b6aad85e7
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-03-30 15:15:30 -0600

    Adds layots

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public/img')));
 
 app.use(routes);
 
