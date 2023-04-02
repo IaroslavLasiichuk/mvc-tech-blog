cc94e4521821d380f356c800bdb1cd7556a2133a
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-04-02 11:34:03 -0600

    Update blog

--- controllers/api/dashboard-routes.js
+++ controllers/api/dashboard-routes.js
@@ -14,7 +14,6 @@ router.post('/', async (req, res) => {
     const newBlog = await Blog.create({
       ...req.body,
     });
-    console.log(req.body)
     res.status(200).json(newBlog);
   } catch (error) {
     res.status(400).json(error);
